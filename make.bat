4cda9350f287846c903d20ecfaf0c93ccf4d435b
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-11-22 23:59:17 +0100

    Jumping

--- make.bat
+++ make.bat
@@ -1,3 +1,5 @@
 xas99.py -R -L pyramarama.lst src/pyjamarama.a99
 
-xas99.py -R -i src/pyjamarama.a99
+xas99.py -R -i src/pyjamarama.a99 -o PYJA
+
+java -jar tools/ea5tocart.jar PYJA "PYJAMARAMA" > make.log

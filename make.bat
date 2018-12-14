64f058dfb35194152e0d1d48985f362989f104c4
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-12-14 21:03:06 +0100

    Compress demo

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@ xas99.py -R -L pyramarama.lst src/pyjamarama.a99
 
 xas99.py -R -i src/pyjamarama.a99 -o PYJA
 
-rem java -jar tools/ea5tocart.jar PYJA "PYJAMARAMA" > make.log
+java -jar tools/ea5tocart.jar PYJA "PYJAMARAMA" > make.log

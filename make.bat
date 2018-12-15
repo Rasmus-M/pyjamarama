a953091e58cb67769abdb2a70d794ed3c7fa2295
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-12-15 14:26:34 +0100

    Updated ea5tocart tool

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@ xas99.py -R -L pyramarama.lst src/pyjamarama.a99
 
 xas99.py -R -i src/pyjamarama.a99 -o PYJA
 
-java -jar tools/ea5tocart.jar PYJA "PYJAMARAMA" > make.log
+java -jar tools/ea5tocart.jar PYJA "PYJAMARAMA" 0 64k > make.log

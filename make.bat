6ba46552056692a440090823691e542539fd946f
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-12-16 14:20:09 +0100

    Make disk

--- make.bat
+++ make.bat
@@ -1,5 +1,16 @@
 xas99.py -R -L pyramarama.lst src/pyjamarama.a99
 
+IF EXIST pyjamarama.dsk GOTO :dskok
+xdm99.py pyjamarama.dsk --initialize DSSD -n PYJAMARAMA
+:dskok
+
 xas99.py -R -i src/pyjamarama.a99 -o PYJA
+
+xdm99.py pyjamarama.dsk -a PYJA
+xdm99.py pyjamarama.dsk -a PYJB
+xdm99.py pyjamarama.dsk -a PYJC
+xdm99.py pyjamarama.dsk -a PYJD
+xdm99.py pyjamarama.dsk -a PYJE
+xdm99.py pyjamarama.dsk -a PYJF
 
 java -jar tools/ea5tocart.jar PYJA "PYJAMARAMA" 0 64k > make.log

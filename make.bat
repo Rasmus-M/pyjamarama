8da0ae924d19ae72e549344c1593cf270a07941d
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-12-22 20:27:50 +0100

    Build

--- make.bat
+++ make.bat
@@ -1,32 +1,46 @@
-xas99.py -R src/pyjamarama.a99
-
 IF EXIST pyjamarama.dsk GOTO :dskok
 xdm99.py pyjamarama.dsk --initialize DSSD -n PYJAMARAMA
 :dskok
 
-xas99.py -R -D supercart -i src/pyjamarama.a99 -o PYJA
+REM Object file
 
-xdm99.py pyjamarama.dsk -a PYJA
-xdm99.py pyjamarama.dsk -a PYJB
-xdm99.py pyjamarama.dsk -a PYJC
-xdm99.py pyjamarama.dsk -a PYJD
-xdm99.py pyjamarama.dsk -a PYJE
+xas99.py -R -L pyramarama32k.lst src/pyjamarama.a99
+
+REM E/A#5 for 32K only
+
+xas99.py -R -i src/pyjamarama.a99 -o PYJA1
+
+xdm99.py pyjamarama.dsk -a PYJA1
+xdm99.py pyjamarama.dsk -a PYJA2
+xdm99.py pyjamarama.dsk -a PYJA3
+xdm99.py pyjamarama.dsk -a PYJA4
+
+REM E/A#5 for 32K + Supercart
+
+xas99.py -R -D supercart -i src/pyjamarama.a99 -o PYJASC1
+
+xdm99.py pyjamarama.dsk -a PYJASC1
+xdm99.py pyjamarama.dsk -a PYJASC2
+xdm99.py pyjamarama.dsk -a PYJASC3
+xdm99.py pyjamarama.dsk -a PYJASC4
+xdm99.py pyjamarama.dsk -a PYJASC5
+
+REM Cartridge + 32K
 
 xas99.py -R -L pyramarama.lst -D supercart cartridge -i src/pyjamarama.a99 -o PYJA
 
 java -jar tools/ea5tocart.jar PYJA "PYJAMARAMA" 0 > make.log
 
-xas99.py -b src/cart-bank-4.a99
+xas99.py -b -o bin/cart-bank-4 src/cart-bank-4.a99
 
-tools\pad.exe cart-bank-4_6100_b4.bin cart-bank-4.bin 7936
+tools\pad.exe bin\cart-bank-4_6100_b4 bin\cart-bank-4.bin 7936
 
 copy /b ^
 PYJA8.bin + ^
-empty256.bin + cart-bank-4.bin + ^
-empty-2k.bin + graphics\WALLY.TIAP + ^
-empty-2k.bin + graphics\WALLY.TIAC + ^
-empty.bin ^
+bin\empty256.bin + bin\cart-bank-4.bin + ^
+bin\empty-2k.bin + graphics\WALLY.TIAP + ^
+bin\empty-2k.bin + graphics\WALLY.TIAC + ^
+bin\empty.bin ^
 pyjamarama-8.bin
 
-:header
 java -jar tools/CopyHeader.jar pyjamarama-8.bin 60 60
